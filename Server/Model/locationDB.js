243d058559f7e7551e97f703ab0e0a52c867c9e3
jyotimane2705 <jyotimane2705@gmail.com>
2024-05-13 16:51:17 +0530

    Zomato Project
    Zomato Clone Project

---
+++ Server/Model/locationDB.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const locationSchema = new Schema({
+    name : {
+        type : String,
+        required : true
+    }
+})
+module.exports = mongoose.model('locationData',locationSchema,'location');
